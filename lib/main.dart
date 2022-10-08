0999f94c99a8e93100f8fa608dc143962e683b59
RahulMachhiC <64777899+RahulMachhiC@users.noreply.github.com>
2022-10-08 12:56:21 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:codetown/screens/loginscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+        //  name: "testapp",
+        // options: DefaultFirebaseOptions.currentPlatform,
+      );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Firebase Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const LoginScreen(),
+      );
+}
